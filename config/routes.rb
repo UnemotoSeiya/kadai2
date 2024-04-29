dca0456ccfbcbfe873379db15d192f556252376d
UnemotoSeiya <164290042+UnemotoSeiya@users.noreply.github.com>
2024-04-29 10:09:47 +0000

    [Add] kadai2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index', as: 'book'
-  get 'books/:id' => 'books#show', as: 'show_book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get '/' => 'homes#top'
-  post 'books' => 'books#create'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'boos/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
